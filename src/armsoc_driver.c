endlessm/xf86-video-armsoc

commit 22437e984ec678245d858d5e7ca98ce441929a95

Call DRI2CloseScreen after pScreen->CloseScreen
Both DRI2 and RandR may wrap pScrn functions.  Thus, it is important that
they get closed in the opposite order that they were initialized.

OMAPScreenInit() calls DRI2ScreenInit() before xf86CrtcScreenInit() (which
in turn calls xf86RandR12Init12).

Therefore, OMAPScreenCloseScreen() must unwrap xf86CrtcCloseScreen() and
call it (which in turn calls xf86RandR12CloseScreen) before calling
DRI2CloseScreen.

This should ensure that wrapped function order is correct between DRI2 and
RandR12.

In particular, this fixes the EnterVT handler such that it is correct on
subsequent server generations.

Cherry-Picked from:
https://chromium.googlesource.com/chromiumos%2Fthird_party%2Fxf86-video-armsoc
c5f79d00aa22fe7d0d2a7453f266a0f17539ebdd

Change-Id: I5665c3452a18bf2009640453f62dda0f33eb215f

src/armsoc_driver.c
@@ -1058,10 +1058,17 @@ ARMSOCCloseScreen(CLOSE_SCREEN_ARGS_DECL)
 {
 	ScrnInfoPtr pScrn = xf86ScreenToScrn(pScreen);
 	struct ARMSOCRec *pARMSOC = ARMSOCPTR(pScrn);
+	Bool ret;
 
 	TRACE_ENTER();
 
 	drmmode_screen_fini(pScrn);
+
+	unwrap(pARMSOC, pScreen, CloseScreen);
+	unwrap(pARMSOC, pScreen, BlockHandler);
+	unwrap(pARMSOC, pScreen, CreateScreenResources);
+
+	ret = (*pScreen->CloseScreen)(scrnIndex, pScreen);
 
 	if (pARMSOC->dri)
 		ARMSOCDRI2CloseScreen(pScreen);
@@ -1080,13 +1087,9 @@ ARMSOCCloseScreen(CLOSE_SCREEN_ARGS_DECL)
 
 	pScrn->vtSema = FALSE;
 
-	unwrap(pARMSOC, pScreen, CloseScreen);
-	unwrap(pARMSOC, pScreen, BlockHandler);
-	unwrap(pARMSOC, pScreen, CreateScreenResources);
-
 	TRACE_EXIT();
 
-	return (*pScreen->CloseScreen)(CLOSE_SCREEN_ARGS);
+	return ret;
 }
 
 

commit c0bf230faa3bdfd8ecbf09531b78484ddbc62ee0

Fix build failure when XF86_SCRN_INTERFACE defined
fixes a problem introduced with commit 22437e9

Change-Id: Ie0150f9e7f7d0507864f2db9e08a23c3200fe9eb

src/armsoc_driver.c
@@ -1068,7 +1068,7 @@ ARMSOCCloseScreen(CLOSE_SCREEN_ARGS_DECL)
 	unwrap(pARMSOC, pScreen, BlockHandler);
 	unwrap(pARMSOC, pScreen, CreateScreenResources);
 
-	ret = (*pScreen->CloseScreen)(scrnIndex, pScreen);
+	ret = (*pScreen->CloseScreen)(CLOSE_SCREEN_ARGS);
 
 	if (pARMSOC->dri)
 		ARMSOCDRI2CloseScreen(pScreen);